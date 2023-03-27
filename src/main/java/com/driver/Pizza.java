81faa955fe17d1eb56c12ed6dfed2ceca511e43e
Prasad24909 <durgaprasad24a@gmail.com>
2023-03-27 13:50:46 +0530

    Solution

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,23 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-
+    boolean isExtraCheeseAdded;
+    boolean isExtraToppingAdded;
+    boolean isBillCreated;
+    boolean isTakeAway;
+    private int toppings;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            this.price = 300;
+            this.toppings = 70;
+        }
+        else {
+            this.price = 400;
+            this.toppings = 120;
+        }
+        this.bill = "Base Price Of The Pizza: "+this.price+"\n";
     }
 
     public int getPrice(){
@@ -17,18 +30,66 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!isExtraCheeseAdded){
+            this.price+=80;
+            isExtraCheeseAdded = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isExtraToppingAdded){
+            this.price+=this.toppings;
+            isExtraToppingAdded = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isTakeAway){
+            this.price+=20;
+            isTakeAway = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        if(!isBillCreated){
+            if(isExtraCheeseAdded){
+                this.bill += "Extra Cheese Added: 80\n";
+            }
+            if(isExtraToppingAdded){
+                this.bill += "Extra Toppings Added: "+this.toppings+"\n";
+            }
+            if(isTakeAway){
+                this.bill += "PaperBag Added: 20\n";
+            }
+            this.bill += "Total Price: "+this.price+"\n";
+            isBillCreated = true;
+        }
         return this.bill;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Boolean getVeg() {
+        return isVeg;
+    }
+
+    public void setVeg(Boolean veg) {
+        isVeg = veg;
+    }
+
+    public void setBill(String bill) {
+        this.bill = bill;
+    }
+
+    public int getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(int toppings) {
+        this.toppings = toppings;
     }
 }
